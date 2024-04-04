cc82bf4b395c67f88a98c7e43439aa1d008f5692
Jaz-3-0 <sebejaz99@gmail.com>
2024-04-04 13:00:43 +0200

    update init

--- mentorship/express/app.js
+++ mentorship/express/app.js
@@ -1,18 +1,36 @@
 const express = require('express');
 const app = express();
+const connect = require('connect');
 
-const port = 8000;
 
-app.get('/', (req, res) => {
+app.get('/https://jonas-sebera.vercel.app', (req, res) => {
     res.json({code: 200, message: 'OK - success'});
     res.json({code: 404, message: 'Not Found'});
     res.json({code: 403, message: 'Forbidden'});
 
 });
 
-app.get('/home', (req, res) => {
+app.get('/https://jonas-sebera.vercel.app/home', (req, res) => {
+    res.send("This is the home page")
+    res.write("This is the body")
 
 });
+
+app.get('/https://jonas-sebera.vercel.app/home/room', (req, res) =>{
+    
+    res.send("This is the room")
+    window.location.reload();
+
+    setTimeout(() => {
+        window.location.reload()
+
+})
+
+});
+
+// connect
+
+
 
 app.listen(8000, () => {
     console.log('Our app is listening at http://localhost:8000...');
